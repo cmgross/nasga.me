a6eae92e1c3ea10cb722ed6eeaf7876dd0ba4c1f
Uziel <chuckgross@gmail.com>
2013-10-25 15:18:28 -0400

    Changed Profile index view to use ViewModel
    Added profile view model and changed form to use this view model.

--- nasga.me/Controllers/ProfileController.cs
+++ nasga.me/Controllers/ProfileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using nasga.me.Models;
 
 namespace nasga.me.Controllers
 {
@@ -10,9 +11,19 @@ namespace nasga.me.Controllers
     {
         // GET: /Profile/
         //http://stackoverflow.com/questions/9158225/using-cookie-in-asp-net-mvc-c-sharp
+        [HttpGet]
         public ActionResult Index()
+        {
+            //pull values from cookies and create a viewmodel with that info
+            //if cookies are null, pass an empty viewmodel
+            var profileViweModel = new ProfileViewModel();
+            return View(profileViweModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(ProfileViewModel profile)
         {
             return View();
         }
-	}
+    }
 }
---
+++ nasga.me/Models/ProfileViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace nasga.me.Models
+{
+    public class ProfileViewModel
+    {
+        [Required(ErrorMessage = "First name required.")]
+        [StringLength(5, ErrorMessage = "First name cannot exceed 5 characters.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last name required.")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Class required.")]
+        public string AthleteClass { get; set; }
+        public List<SelectListItem> Classes { get; set; }
+
+        public ProfileViewModel(string firstName = "", string lastName = "", string className = "")
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            //put this in webconfig
+            var classes = new List<string> {"", "Pro", "Masters", "Pro+Masters", "Amateur", "Womens"};
+            Classes = classes.Select(p => new SelectListItem
+            {
+                Text = p,
+                Value = p,
+                Selected = p == className
+            }).ToList();
+        }
+
+        public ProfileViewModel()
+        {
+            var classes = new List<string> { "", "Pro", "Masters", "Pro+Masters", "Amateur", "Womens" };
+            Classes = classes.Select(p => new SelectListItem
+            {
+                Text = p,
+                Value = p,
+                Selected = p == ""
+            }).ToList();
+        }
+    }
+}
