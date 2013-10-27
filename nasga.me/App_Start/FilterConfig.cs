13a51bd3fb6bde906004ad28010bb7cb58e5a651
Uziel <chuckgross@gmail.com>
2013-10-27 10:34:56 -0400

    Removed error handler attribute from RegisterGlobalFilters
    Did this so AppHarbor can show any errors

--- nasga.me/App_Start/FilterConfig.cs
+++ nasga.me/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace nasga.me
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
         }
     }
 }
