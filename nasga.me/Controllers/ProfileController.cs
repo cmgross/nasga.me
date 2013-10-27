01b2a164417e6fd58cea8e570218dc9fe71bb3e3
Uziel <chuckgross@gmail.com>
2013-10-26 20:57:17 -0400

    Added getting/setting of cookies

--- nasga.me/Controllers/ProfileController.cs
+++ nasga.me/Controllers/ProfileController.cs
@@ -14,16 +14,33 @@ namespace nasga.me.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            //pull values from cookies and create a viewmodel with that info
-            //if cookies are null, pass an empty viewmodel
-            var profileViweModel = new ProfileViewModel();
+            HttpCookie firstNameCookie = HttpContext.Request.Cookies["nasga_me_fname"];
+            HttpCookie lastNameCookie = HttpContext.Request.Cookies["nasga_me_lname"];
+            HttpCookie athleteClassCookie = HttpContext.Request.Cookies["nasga_me_class"];
+            string firstName = firstNameCookie == null ? "" : firstNameCookie.Value;
+            string lastName = lastNameCookie == null ? "" : lastNameCookie.Value;
+            string athleteClass = athleteClassCookie == null ? "" : athleteClassCookie.Value;
+            ProfileViewModel profileViweModel = new ProfileViewModel(firstName, lastName,athleteClass);
             return View(profileViweModel);
         }
 
         [HttpPost]
         public ActionResult Index(ProfileViewModel profile)
         {
-            return View(profile);
+            //todo anti forgery token, if modelstate is valid, etc
+            HttpCookie firstNameCookie = new HttpCookie("nasga_me_fname") { Value = profile.FirstName };
+            HttpCookie lastNameCookie = new HttpCookie("nasga_me_lname") { Value = profile.LastName };
+            HttpCookie athleteClassCookie = new HttpCookie("nasga_me_class") { Value = profile.AthleteClass };
+
+            HttpContext.Response.Cookies.Remove("nasga_me_fname");
+            HttpContext.Response.Cookies.Remove("nasga_me_lname");
+            HttpContext.Response.Cookies.Remove("nasga_me_class");
+
+            HttpContext.Response.SetCookie(firstNameCookie);
+            HttpContext.Response.SetCookie(lastNameCookie);
+            HttpContext.Response.SetCookie(athleteClassCookie);
+
+            return RedirectToAction("Index");
         }
     }
 }
